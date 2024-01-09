f33e1ab6e5b1887d28e6863c8198f673a14883d5
hui-12 <laiyh091204@gmail.com>
2024-01-09 17:33:07 +0800

    change ouput mode

--- user.h
+++ user.h
@@ -9,63 +9,40 @@ void user_page(){
     int input;
 
     USER:
-    cout<<endl;
-    cout<<"========================="<<endl;
-    cout<<"========================="<<endl;
-    cout<<"\tUSER MENU"<<endl<<endl;
-    cout<<"Welcome to TechMart!!!"<<endl<<endl;
-    cout<<"1. Profile"<<endl;
-    cout<<"2. Categories"<<endl;
-    cout<<"3. Price Catalogue"<<endl;
-    cout<<"4. Cart"<<endl;
-    cout<<"5. Log out"<<endl;
-    cout<<"Please enter a number (1-5)."<<endl;
-    cout<<"========================="<<endl;
-    cout<<"========================="<<endl;
+    cout<< R"(
+    =========================
+    =========================
+    USER MENU
+
+    Welcome to TechMart!!!
+    1. Profile
+    2. Categories
+    3. Price Catalogue
+    4. Cart
+    5. Log out
+    Please enter a number (1-5).
+    =========================
+    =========================
+    )";
     cin >> input;
 
-    switch(input){
-        case 1 :
-
-        break;
-
-        case 2 :
-
-        break;
-
-        case 3 :
-
-        break;
-
-        case 4 :
-
-        break;
-
-        case 5 :
-        cout<<"Thank You!"<<endl;
-
-        break;
-
-        default :
-        cout<<"Invalid number! Please input again.";
-        goto USER;
-    }
 }
 
 void profile(){
 int input;
 
-    cout<<endl;
-    cout<<"========================="<<endl;
-    cout<<"========================="<<endl;
-    cout<<"\tProfile Page"<<endl<<endl;
-    cout<<"Welcome to TechMart!!!"<<endl<<endl;
-    cout<<"1. Shop Location"<<endl;
-    cout<<"2. Modify"<<endl;
-    cout<<"3. Log Out"<<endl;
-    cout<<"Please enter a number (1-3)."<<endl;
-    cout<<"========================="<<endl;
-    cout<<"========================="<<endl;
+    cout<< R"(
+    =========================
+    =========================
+    Profile Page
+
+    1. Shop Location
+    2. Modify
+    3. Log Out
+    Please enter a number (1-3).
+    =========================
+    =========================
+    )";
     cin >> input;
 
 }
