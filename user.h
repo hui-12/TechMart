d3e6f0c88f8f84d9934df0c9213974f9e2fd539f
ahSee1901 <seeyongjun0119@e.newera.edu.my>
2024-01-11 10:19:01 +0800

    user.h update

--- user.h
+++ user.h
@@ -60,9 +60,60 @@ void categories(){
     1. Processor
     2. Ram Value
     3. Keyboard
+
     4. Return
 
     Please enter a number (1-4).
+    =========================
+    =========================
+    )";
+    cin >> input;
+
+}
+
+void supplierProfile(){
+    int input;
+    
+    cout<< R"(
+    =========================
+    =========================
+    Supplier Profile
+
+    1. XIXI TECHNOLOGY
+    2. HAHA TECHNOLOGY
+    3. HIHI TECHNOLOGY
+
+    4. Return
+
+    Please enter a number (1-4).
+    =========================
+    =========================
+    )";
+    cin >> input;
+
+}
+
+void productList(){
+    int input;
+    
+    cout<< R"(
+    =========================
+    =========================
+    Product List
+
+    1. INTEL CORE I5 10500
+    2. INTEL CORE I7 10700K
+    3. INTEL CORE I9 10900X
+    4. AORUS PC3333
+    5. AORUS PC3733
+    6. AORUS PC4400
+    7. CYNOSA CHROMA MEMBRANE
+    8. BLK WIDOW ULTIMATE 2016
+    9. BLK WIDOW TE VHROMA V2 QUARTZ EDI
+
+    10. Return
+
+    Please enter a number (1-10).
     =========================
     =========================
     )";
